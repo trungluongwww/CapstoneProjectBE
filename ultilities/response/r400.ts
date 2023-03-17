eebff4256f6b4add418965957946bbccd3097513
trungluong <trungluong>
2023-03-17 17:15:04 +0700

    add something

--- ultilities/response/r400.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from "express";
-
-export default (
-  res: Response,
-  data: any = null,
-  message: string = "thất bại"
-) => {
-  return res.status(400).json({ data, message });
-};
--- ultilities/strings/array.ts
+++
@@ -1,7 +0,0 @@
-const include = (arr: Array<string>, a: string): boolean => {
-  return arr.includes(a);
-};
-
-export default {
-  include,
-};
