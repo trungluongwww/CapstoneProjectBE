eddd1e493ecd4490f11efbe105856e87d14d49e1
trungluong <trungluong>
2023-07-09 16:12:22 +0700

    fix

--- server_admin/services/convenience/create.ts
+++ server_admin/services/convenience/create.ts
@@ -2,8 +2,13 @@ import { Convenience } from "../../../modules/database/entities";
 import pmongo from "../../../external_node/ultils/pmongo";
 import dao from "../../dao";
 import { IConvenienceCreatePayload } from "../../../internal/interfaces/convenience";
+import errorCode from "../../../internal/error-code";
 
 const fromClient = async (payload: IConvenienceCreatePayload): Promise<Error | null> => {
+  if ((await dao.convenience.find.countByCode(payload.code)) > 0) {
+    return Error(errorCode.convenience.convenience_already_exist);
+  }
+
   const doc = new Convenience();
   doc.id = pmongo.newStringId();
   doc.code = payload.code;
